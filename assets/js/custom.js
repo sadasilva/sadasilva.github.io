956c3ca91be52aaace51a15e4e81ffe3a8b05dc7
Sara <sadasilva@umass.edu>
2015-02-25 05:00:43 -0500

    move js outside

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -102,8 +102,8 @@ jQuery(document).ready(function($){
 					// $.post('email_process.php', $("#contact_form").serialize(), function(data) { // action file is here
 					// 	$('#post_message').html(data);
 					// });
-					$('#thankyouModal').modal('show');
-					// $("#contact_form").reset();
+					// $('#thankyouModal').modal('show');
+					$("#contact_form").reset();
 				}
 
 		//
